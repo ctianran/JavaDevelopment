52c90242b5ab4bdfbf3766b1b89c95b8b0ac8a30
Tianran Chen <tristan20177@gmail.com>
2017-02-12 18:28:51 -0800

    add files

--- Threads/src/com/example/AnotherThread.java
+++ Threads/src/com/example/AnotherThread.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.example.ThreadColor.ANSI_BLUE;
 
 /**
@@ -13,55 +8,15 @@ import static com.example.ThreadColor.ANSI_BLUE;
 public class AnotherThread extends Thread {
 	@Override
 	public void run() {
-		System.out.println(ANSI_BLUE + "Hello from another thread.");
-	}
-}
+		System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
 
-
-public class Solution {
-
-	static class MyComparator implements Comparator<Integer> {
-		private Map<Integer, Integer> map;
-
-		public MyComparator(int[] array) {
-			map = new HashMap<>();
-			for (int i = 0; i < array.length; i++) {
-				map.put(array[i], i);
-			}
+		try {
+			Thread.sleep(3000);
+		} catch(InterruptedException e) {
+			System.out.println(ANSI_BLUE + "Another thread woke up");
+			return;
 		}
 
-		@Override
-		public int compare(Integer i1, Integer i2) {
-			Integer index1 = map.get(i1);
-			Integer index2 = map.get(i2);
-			if (index1 != null && index2 != null) {
-				return index1.compareTo(index2);
-			} else if (index1 == null && index2 == null) {
-				return i1.compareTo(i2);
-			}
-			return index1 != null ? -1 : 1;
-		}
-	}
-
-	public int[] sortSpecial(int[] A1, int[] A2) {
-		Integer[] refArray = toIntegerArray(A1);
-		Arrays.sort(refArray, new MyComparator(A2));
-		toIntArray(refArray, A1);
-		return A1;
-	}
-
-	private void toIntArray(Integer[] array, int[] result) {
-		for (int i = 0; i < array.length; i++) {
-			result[i] = array[i];
-		}
-	}
-
-		private Integer[] toIntegerArray ( int[] array){
-			Integer[] result = new Integer[array.length];
-			for (int i = 0; i < array.length; i++) {
-				result[i] = array[i];
-			}
-			return result;
-		}
+		System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
 	}
 }
